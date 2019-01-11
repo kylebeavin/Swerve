03b033e30d634c6bc35423b05b108b4e614d335d
kyle beavin <kylebeavin@gmail.com>
2019-01-10 19:17:17 -0500

    init commit

---
+++ screens/KillaScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class KillaScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Killa',
+    };
+
+    render() {
+        return(
+            <View>
+                <Text>Hello World</Text>
+            </View>
+        );
+    }
+} 
+export default KillaScreen;
