6f0056e4e692c2d7ea0e5b54a19c58bc31ce88b2
kyle beavin <kylebeavin@gmail.com>
2019-01-15 23:14:50 -0500

    fetch maps

--- screens/KillaScreen.js
+++ screens/KillaScreen.js
@@ -1,16 +1,51 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { MapView } from "expo";
 
 class KillaScreen extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoading: true,
+            markers: [],
+        }
+    }
     static navigationOptions = {
         title: 'Killa',
     };
 
+    componentDidMount() {
+        this.fetchMarkerData();
+    }
+
+    fetchMarkerData() {
+        fetch('https://feeds.citibikenyc.com/stations/stations.json')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({
+                    isLoading: false,
+                    markers: responseJson.stationBeanList,
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
     render() {
         return(
-            <View>
-                <Text>Hello World</Text>
-            </View>
+            <MapView
+            style={{
+              flex: 1
+            }}
+            initialRegion={{
+              latitude: 39.768402,
+              longitude: -86.158066,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421
+            }}
+          />
         );
     }
 } 
